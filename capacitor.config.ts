0d068f45792d58cb281b91eb4ae89f1c9c9fcad7
maximetassaert <maxime.tas@outlook.fr>
2023-06-06 08:19:19 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ma crypto',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
